56129db7f6229f6333774ef2c3fd72deba831d3a
rejaul <rejaulvoice@gmail.com>
2024-12-01 16:54:54 +0000

    Ready to deploy in vercel

---
+++ index.js
@@ -0,0 +1,147 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.send.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+// coffeeMaster
+// e5uZtCpU6vgE8X75
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omdcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // client.connect() nam a je connection toyari hoiche oikhane coffeeDB nam a ekta database create hobe
+    const coffeeCollection = client.db('coffeeDB').collection('coffee')
+    //collection means data gulu coffee object nam a database a save hobe
+
+    // users collection
+    const userCollection = client.db('coffeeDB').collection('users');
+
+    // to get the data in clent side data endpoint
+    app.get('/coffee', async(req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      // promise ta paile send korbo
+      res.send(result)
+    })
+
+    // This route/endpoint to get one data by id to update data
+    app.get('/coffee/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    })
+    // data receiving from coffee table
+    app.post('/coffee', async(req, res)=>{
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      // response ta paile ta client side a send kore dichhi
+      res.send(result);
+    })
+
+    // update a specific data dynamic route
+    app.put('/coffee/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+      //req.body theke data ta form theke niye updatedCoffee variable a rakha hochhe
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo
+        }
+      }
+      const result = await coffeeCollection.updateOne(filter, coffee, options)
+      res.send(result);
+    })
+
+    // Delete route
+    app.delete('/coffee/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    //Users related apis
+    //get api
+    app.get('/users', async(req, res)=>{
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async(req, res)=>{
+      const newUser = req.body;
+      console.log('creating new user', newUser);
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    })
+
+    // we will use patch when need to modify existing data
+    app.patch('/users', async(req, res)=>{
+      const email = req.body.email;
+      const filter = {email};
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: req.body?.lastSignInTime
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    // delete user api
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res)=>{
+    res.send('Coffee making server is running');
+})
+
+app.listen(port, ()=>{
+    console.log(`Coffee server is running on port: ${port}`)
+})
---
+++ notes.js
@@ -0,0 +1,23 @@
+/**
+ * dotenv er mdadhome secret cradential hide korte hobe
+ * finally .gitignore file er vitor .env er node_modules dite hobe file ignore korar jonno duringh git push
+ * delete deuar por ta home page theke sore jachhe nah theke jachee
+ * tar jonno mechanism korte hobe
+ */
+
+
+/**
+ * hosting
+ * 
+ * 1. firebase-tools (one time)
+ * 2. firebase login (one time)
+ * 3. one time for each project: firebase init
+ * 4. what you want to do: select hosting
+ * 5. select a project (already have one created in firebase console)
+ * 6. what will be our public -------- dist
+ * 7. single page?: yes
+ * 
+ * ###### each time you want to deploy
+ * 8. npm run build
+ * 9. firebase deploy
+ */
